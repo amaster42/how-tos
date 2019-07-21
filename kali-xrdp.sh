521340c51530bc28974ab2a63e33a8948e8d161a
amaster42 <23503669+amaster42@users.noreply.github.com>
2019-07-21 12:42:22 -0400

    Add files via upload

--- kali-xrdp.sh
+++ kali-xrdp.sh
@@ -19,7 +19,7 @@ echo "[+] Starting xrdp service and enabling on boot"
 #echo "[+] Configuring xrdp to listen on port 3390 rather than default"
 #sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
 sudo echo 'xfce4-session' > ~/.xsession
-sudo echo 'startxfce4' >> /etc/xrdp/startwm.sh
+echo 'startxfce4' | sudo tee -a /etc/xrdp/startwm.sh
 #sudo sed -i 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 sudo systemctl enable xrdp-sesman #persist through reboots
 sudo service xrdp restart
